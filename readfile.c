d85468d0218ccce4e23b9f4bf0b7ae26c0c88678
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-22 14:32:31 -0800

    Clean up file formatting.

--- readfile.c
+++ readfile.c
@@ -15,21 +15,23 @@ int main(int argc, const char *argv[])
         exit(0);
     }
 
-    /** Use syscall to read input file in read-only mode */
+    /* Use syscall to read input file in read-only mode */
     int file_descriptor = open(argv[1], O_RDONLY, 0);
 
-    /** Throw error if specified file doesn't exist */
+    /* Throw error if specified file doesn't exist */
     if (file_descriptor < 0)
     {
         perror("Specified file does not exist");
         exit(0);
     }
 
-    /** Allocate enough space to contain the file contents in memory. */
+    /* Allocate enough space to contain the file contents in memory. */
     char *contents = (char *)malloc(2048);
     int bytes = read(file_descriptor, contents, 2048);
 
-    // write the contents to standard output
+    /* Write the contents to standard output */
     write(1, contents, bytes);
+
+    /* Close the file */
     close(file_descriptor);
 }
