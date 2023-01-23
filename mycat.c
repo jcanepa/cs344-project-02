55fff9f5d84689f74a8911ebc37abfa58c6759f0
Julian Canepa <julian.l.canepa@gmail.com>
2023-01-22 21:46:23 -0800

    Commit buggy code, write returns more than expected stdout.

--- mycat.c
+++ mycat.c
@@ -3,30 +3,14 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-// did the user specify a (maybe filename) argument in the command?
-// Yes argc is 1 or greater, loop through them all
-// arg at index i is a valid filename
-
-int main(int argc, const char *argv[])
+void read_write_from_file(const char *filename)
 {
-    // read from standard input
-    if (argc == 1)
-    {
-        char buffer[2048];
-        while (read(0, buffer, sizeof(buffer)) > 0)
-        {
-            printf("%s", buffer);
-        }
-        exit(0);
-    }
-
-    // otherwise, process each arg as a filename to cat.
     int file_descriptor;
     char *contents;
     int bytes;
 
     /* Use syscall to read input file in read-only mode */
-    file_descriptor = open(argv[1], O_RDONLY, 0);
+    file_descriptor = open(filename, O_RDONLY, 0);
 
     /* Throw error if specified file doesn't exist */
     if (file_descriptor == -1)
@@ -44,4 +28,26 @@ int main(int argc, const char *argv[])
 
     /* Close the file */
     close(file_descriptor);
+}
+
+int main(int argc, const char *argv[])
+{
+    char *buffer = (char *)malloc(2048);
+
+    // no arguments provided
+    if (argc == 1)
+    {
+        // read from standard input
+        while (read(0, buffer, sizeof(buffer)) != 0)
+        {
+            write(1, buffer, 2048);
+        }
+        exit(0);
+    }
+
+    // otherwise, process each supplied arg as a filename
+    for (int i = 0; i < argc; i++)
+    {
+        read_write_from_file(argv[i]);
+    }
 }
